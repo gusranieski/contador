0e40cc5a4281aa167dc8548d061a08c6f486098c
gustavo.rañieski <gustavoranieski@hotmail.com>
2023-02-13 10:02:52 -0300

    segundo commit: agregué estilos

--- scripts.js
+++ scripts.js
@@ -1,33 +1,3 @@
-// const btnStart = document.querySelector("#startCounter");
-// const btnStop = document.querySelector("#stopCounter");
-// const btnReset = document.querySelector("#resetCounter");
-// const span = document.querySelector("span");
-// let count = 0;
-// let counter;
-
-// btnStart.addEventListener("click", () => {
-//   btnStart.innerHTML = "Start";
-//   counter = setInterval(() => {
-//     count++;
-//     span.innerHTML = count;
-
-//     if (count === 100) {
-//       count = 0;
-//     }
-//   }, 1000);
-// });
-
-// btnStop.addEventListener("click", () => {
-//   clearInterval(counter);
-//   btnStart.innerHTML = "Continue";
-// });
-
-// btnReset.addEventListener("click", () => {
-//   count = 0;
-//   span.innerHTML = count;
-//   btnStart.innerHTML = "Start";
-// });
-
 const btnStart = document.querySelector("#startCounter");
 const btnStop = document.querySelector("#stopCounter");
 const btnReset = document.querySelector("#resetCounter");
@@ -41,7 +11,7 @@ btnStart.addEventListener("click", () => {
     counter = setInterval(() => {
       count++;
       span.innerHTML = count;
-  
+
       if (count === 100) {
         count = 0;
       }
